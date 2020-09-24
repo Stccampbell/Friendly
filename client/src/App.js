32e83a07b889b95c97dfb008b7cc18a9bfaaf8fa
spencer campbell <stccampbell@gmail.com>
2020-09-24 13:06:23 -0500

    added bootstrap and the chat message component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import VideoChat from './components/VideoChat';
+import TextChat from './components/TextChat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <VideoChat/>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+            {/* <VideoChat/> */}
+            <TextChat />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
